a8872c5011e4927e4c633d0fc8a0594de190552b
Jacob Miecznikowski <jmieczni@purdue.edu>
2017-07-11 10:29:51 -0400

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Route, Switch, NavLink} from 'react-router-dom'
+
 import './App.css'
 
 class App extends Component {
@@ -10,7 +12,16 @@ class App extends Component {
             <h3>Ain't no parity like an</h3>
             <h1>API Parity</h1>
           </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to='/github'>Github API</NavLink>
+            </li>
+          </ul>
         </div>
+        <Switch>
+          <Route path='/github' render={() => <h1>Github</h1>} /> 
+          <Route render={() => <p>To get started, click an above link</p>} />
+        </Switch>
       </div>
     )
   }
