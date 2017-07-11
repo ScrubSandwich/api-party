b1e0b25e0feffcb7b6820cabcb397e9369bb3132
Jacob Miecznikowski <jmieczni@purdue.edu>
2017-07-11 11:29:16 -0400

    Add user component

--- src/Github.js
+++ src/Github.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import GithubUser from './GithubUser'
 import './Github.css'
+
 
 class Github extends Component {
   state = {
@@ -37,6 +39,7 @@ class Github extends Component {
           </div>
         </form>  
         <Route exact path="/github" render={() => <h3>Please enter a username to search on Github</h3>} />
+        <Route path="/github/:username" component={GithubUser} />
       </div>
     )
   }
