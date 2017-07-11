5bacc2ed0b8533dc90bac160cd9e42ceb32519fb
Jacob Miecznikowski <jmieczni@purdue.edu>
2017-07-11 11:02:37 -0400

    Link the form to state

--- src/Github.js
+++ src/Github.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
+import './Github.css'
 
 class Github extends Component {
+  state = {
+    username: "",
+
+  }
+
+  handleChange = (ev) => {
+    this.setState({
+      username: ev.target.value
+    }, () => console.log('State: ' + this.state.username))
+  }
+
   render() {
     return (
-      <h1>Githubs</h1>
+      <div className="github">
+        <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github logo"/>
+        <form>
+          <div>
+            <input 
+              type="text" 
+              placeholder="Search for a username" 
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </div>  
+          <div>
+            <button>Look up GitHub user</button>
+          </div>
+        </form>        
+      </div>
     )
   }
 }
